5134cacd2cd6aee3beef2b95c9095a9d59e99505
Ian Farr <Ian.Farr@farrworks.com>
2025-08-16 21:53:04 -0400

    adds docker cleanup steps to run_container

--- run_container.sh
+++ run_container.sh
@@ -4,13 +4,15 @@ docker stop anythingllm
 
 docker rm anythingllm
 
+docker system prune -a
+docker volume prune
+
 docker pull mintplexlabs/anythingllm:latest
 
 
 docker run -d --restart unless-stopped --name anythingllm \
   --memory="3g" \
   --memory-swap="4g" \
-  --cpus="2" \
   --shm-size="1g" \
   --cap-add SYS_ADMIN \
   -p 3001:3001 \
